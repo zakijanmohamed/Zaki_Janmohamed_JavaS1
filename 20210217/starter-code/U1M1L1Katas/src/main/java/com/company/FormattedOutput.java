e298befc93645b15e9229f96cc39420e8bca90ee
zakijanmohamed <78564189+zakijanmohamed@users.noreply.github.com>
2021-02-16 22:31:35 -0600

    2/17/2021

---
+++ 20210217/starter-code/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class FormattedOutput {
+
+    public static void main(String[] args) {
+        System.out.format("Ny name is %s and I am %d years old.","Zaki",27);
+
+    }
+}
